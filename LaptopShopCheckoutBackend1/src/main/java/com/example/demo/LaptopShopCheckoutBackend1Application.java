1ef370927b89355928b644c1ab378b3ba6a409af
Mohini Surwase <mohinisurwase03@gmail.com>
2023-04-10 11:06:42 +0530

    first commit

---
+++ LaptopShopCheckoutBackend1/src/main/java/com/example/demo/LaptopShopCheckoutBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopShopCheckoutBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopShopCheckoutBackend1Application.class, args);
+	}
+
+}
